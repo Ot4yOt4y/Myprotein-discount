52084e7b3a09a0a91f2605c659f2dc5a2b187f7e
Ot4yOt4y <sk2680@student.uni-lj.si>
2025-01-01 21:37:13 +0100

    Extracting current discount code

--- myprotein.py
+++ myprotein.py
@@ -1,4 +1,3 @@
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -24,7 +23,7 @@ class MyProteinScraper:
     def driver_setup(self):
         options = Options()
 
-        options.add_argument("--disable-blink-features=AutomationControlled")  #prevents selenium detection
+        options.add_argument("--disable-blink-features=AutomationControlled")  # Prevent Selenium detection
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--start-maximized")
@@ -37,13 +36,18 @@ class MyProteinScraper:
 
     def sign_in(self):
         try:
+            #print(self.driver.current_url})
             print("Opening the sign-in page")
             self.driver.get(self.login_url)
 
-            #waits for email field to show up
+
+            #waiting for email field to show up
             name_input = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, '//input[@name="E-poštni naslov"]'))
             )
+
+            name_input.clear();
+            pyperclip.copy("")  #Clear the clipboard
             #actions = ActionChains(self.driver)
             for chr in email:
                 if chr == "@":
@@ -53,22 +57,32 @@ class MyProteinScraper:
                     name_input.send_keys(Keys.CONTROL, "v")
                 else:
                     name_input.send_keys(chr)
+
             print("Username entered")
 
-            #waits for password field to show up and enters password
+
+            #waiting for password field to show up and entering password
             password_input = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, '//input[@name="Geslo"]'))
             )
             password_input.send_keys(self.password)
             print("Password entered")
 
-            #clicks on "Prijava" button
+
+            #clicking on "Prijava" button
             prijava_button = WebDriverWait(self.driver, 10).until(
                 EC.element_to_be_clickable((By.XPATH, '//button[@data-testid="button-submit-login"]'))
             )
             prijava_button.click()
-            print("Clicked on 'Prijava' button")
-            
+            print("'Prijava' button has been clicked")
+
+            '''
+            #check if account page is loaded
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//h1[contains(text(), "domača stran računa")]'))
+            )
+            '''    
+            #print(self.driver.current_url)
             time.sleep(10)
             if("/accountHome.account" in self.driver.current_url):
                 print("Successfully signed in!")       
@@ -80,11 +94,90 @@ class MyProteinScraper:
             #self.driver.quit()
 
 
+    def go_to_basket(self):
+        #time.sleep(10)
+        try:
+            print("Getting to the basket page")
+
+            #locate and click basket button
+            basket_button = WebDriverWait(self.driver, 20).until(
+                EC.element_to_be_clickable((By.XPATH, '//a[@href="/my.basket"]'))
+            )
+
+            #for stale element exception
+            #basket_button = self.driver.find_element(By.XPATH, '//a[@href="/my.basket"]')
+            
+            basket_button.click()
+            print("Clicked the basket button")
+            
+            time.sleep(10)
+            #print(self.driver.current_url)
+            if("/my.basket" in self.driver.current_url):
+                print("Basket page loaded successfully")
+
+        except Exception as e:
+            print(f"Error while going to the basket page: {e}")
+            
+            
+    def input_code(self):
+        try:
+            #input promo code
+            promo_code_bracket = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//input[@class="athenaBasket_discountEntryInput"]'))
+            )
+            promo_code_bracket.send_keys("PIAFIT")
+            
+            #wait till overlay disappears
+            WebDriverWait(self.driver, 20).until(
+                EC.invisibility_of_element((By.CLASS_NAME, "responsiveFlyoutBasket_overlay"))
+            )
+            
+            #click on "unovči kodo" button
+            unocvi_kodo_button = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, '//button[@class="athenaBasket_discountEntryButton"]'))
+            )
+            unocvi_kodo_button.click()
+                    
+        except Exception as e:
+            print(f"An error while inputing discount code: {e}")
+            
+        
+    def extract_discount_percentage(self):
+        try:
+            #wait for the discount to appear
+            discount = WebDriverWait(self.driver, 15).until(
+                EC.presence_of_element_located((By.CLASS_NAME, "athenaBasket_totalSavingsMessage"))
+            )
+
+            discount_text = discount.text
+            #print(discount_text)
+
+            #extract the discount percentage
+            match = re.search(r"(\d+)%", discount_text)
+            if match:
+                discount_percentage = int(match.group(1))  #to integer
+                print(f"Discount Percentage: {discount_percentage}%")
+                return discount_percentage
+            else:
+                print("No discount percentage found")
+                return None
+
+        except Exception as e:
+            print(f"Error while extracting the discount: {e}")
+
+            return None
+    
+    
     def run(self):
         self.driver_setup()
         self.sign_in()
-       
-       
+        self.go_to_basket()
+        self.input_code()
+        
+        discount_percentage = self.extract_discount_percentage()
+        if(discount_percentage):
+            print(discount_percentage)
+
 if __name__ == "__main__":
     
     '''Replace these two with your own email and password'''
